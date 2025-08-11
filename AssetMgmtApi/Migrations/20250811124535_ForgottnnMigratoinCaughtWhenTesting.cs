b7f3add76e52c82e2cd8946d111853dda4f37aab
smileX <120062084+hassen396@users.noreply.github.com>
2025-08-11 15:47:31 +0300

    Add forgotten pending  migration

---
+++ AssetMgmtApi/Migrations/20250811124535_ForgottnnMigratoinCaughtWhenTesting.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetMgmtApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ForgottnnMigratoinCaughtWhenTesting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetRequests_UserId",
+                table: "AssetRequests",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AssetRequests_AspNetUsers_UserId",
+                table: "AssetRequests",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AssetRequests_AspNetUsers_UserId",
+                table: "AssetRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AssetRequests_UserId",
+                table: "AssetRequests");
+        }
+    }
+}
